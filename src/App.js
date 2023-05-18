401944e1936bc4134f0fb291647d3f696f1134e5
CiscoMerchan <franciscoeliasm@yahoo.com>
2023-05-18 10:29:49 +1000

    Seting App.css

--- src/App.js
+++ src/App.js
@@ -4,7 +4,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      <div className='contenedor-logo'>
+      <h1 className='logo'>calculadora react</h1>
+      </div>
+      <div className='contenedor-calculadora'>
+
+      </div>
     </div>
   );
 }
