ca291e703ab3a33f4731d9a23929da0a38bdec9f
CiscoMerchan <franciscoeliasm@yahoo.com>
2023-05-18 11:43:17 +1000

    Button values

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,13 @@
 
 function Button(props){
+
+  const esOperador = valor => {
+    // isNaN if is a number. = if valor is a number
+    return isNaN(valor) && (valor != '.') && (valor != '=');
+  };
+
     return(
-      <div>
+      <div className={`boton-contenedor ${esOperador(props.children) ? 'operador' : null}`}>
         {props.children}
       </div>        
     );
